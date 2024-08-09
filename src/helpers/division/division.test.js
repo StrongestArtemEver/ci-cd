fdf71c6ab79329902d2f3376e47bd2cc41c17491
Fadin Artem <a.fadin-forwork@mail.ru>
2024-08-09 05:56:12 +0500

    add: division test

---
+++ src/helpers/division/division.test.js
@@ -0,0 +1,13 @@
+import { division } from "./division";
+
+describe('division', function(){
+  test('positive', () => { 
+    expect(division(5,5)).toBe(1);
+   })
+  test('negative', () => { 
+    expect(division(-5,-5)).toBe(1);
+   })
+  test('neutral', () => { 
+    expect(division(5,-5)).toBe(-1);
+   })
+})
